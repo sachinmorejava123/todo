752c339ffa2efcf9018be65d11e58c51e9c1bec1
sachin more <sachin.more@polyglots.io>
2020-09-19 15:47:00 +0530

    css changes for list-grid

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -3,7 +3,11 @@ import { TodoDataService } from '../service/data/todo-data.service';
 import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 export interface PeriodicElement {
   name: string;
@@ -11,30 +15,6 @@ export interface PeriodicElement {
   weight: number;
   symbol: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-    {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-    {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-    {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-    {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-    {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-    {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-    {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-    {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-    {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-    {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-  ];
-
 export class Todo {
   constructor(
     public id: number,
@@ -46,15 +26,22 @@ export class Todo {
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
-  styleUrls: ['./list-todos.component.scss']
+  styleUrls: ['./list-todos.component.scss'],
 })
 export class ListTodosComponent implements OnInit {
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
-  displayedColumns: string[] = ['id', 'description', 'targetDate', 'isCompleted', 'update', 'delete'];
+  displayedColumns: string[] = [
+    'id',
+    'description',
+    'targetDate',
+    'isCompleted',
+    'update',
+    'delete',
+  ];
   todoData: Todo[];
   dataSource: any;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   todos: Todo[];
   message: any;
 
@@ -62,7 +49,7 @@ export class ListTodosComponent implements OnInit {
     private todoService: TodoDataService,
     private router: Router,
     private snackBar: MatSnackBar
-    ) { }
+  ) {}
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<Todo>(this.todoData);
@@ -71,24 +58,20 @@ export class ListTodosComponent implements OnInit {
   }
 
   retriveAllTodos() {
-    this.todoService.retriveAllTodos().subscribe(
-      response => {
-        this.todos = response;
-        this.todoData = response;
-        this.dataSource = new MatTableDataSource<Todo>(this.todoData);
-        this.dataSource.paginator = this.paginator;
-      }
-    );
+    this.todoService.retriveAllTodos().subscribe((response) => {
+      this.todos = response;
+      this.todoData = response;
+      this.dataSource = new MatTableDataSource<Todo>(this.todoData);
+      this.dataSource.paginator = this.paginator;
+    });
   }
 
   deleteTodo(id) {
-    this.todoService.deleteTodo(id).subscribe(
-      resp => {
-        this.message = `Delete of Todo ${id}`;
-        this.openSnackBar();
-        this.retriveAllTodos();
-      }
-    );
+    this.todoService.deleteTodo(id).subscribe((resp) => {
+      this.message = `Delete of Todo ${id}`;
+      this.openSnackBar();
+      this.retriveAllTodos();
+    });
   }
 
   updateTodo(id) {
@@ -106,5 +89,4 @@ export class ListTodosComponent implements OnInit {
       verticalPosition: this.verticalPosition,
     });
   }
-
 }
